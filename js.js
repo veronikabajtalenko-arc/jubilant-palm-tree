1ff805c447bdedde39b9a2e4a9649776ef8447fa
Veronika <veronikabajtalenko@gmail.com>
2026-07-13 20:55:23 +0300

    commit 2

---
+++ js.js
@@ -0,0 +1,11 @@
+function toggleAswer(id){
+
+
+const answer = document.getElementById(id);
+if (answer.style.display==='none'|| !answer.style.display)
+{answer.style.display='block';}
+else {
+    answer.style.display='none';
+}
+
+}
